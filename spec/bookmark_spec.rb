a3480a3d557a9c06ad9740892345567add7ca5e5
Michael Chan <michaelcychan@Michaels-MacBook-Air.local>
2022-06-08 23:36:23 +0100

    updated bookmark_spec unit test

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -6,15 +6,15 @@ describe '.all' do
   it 'returns a list of bookmarks' do
     # connect to testing DB and insert testing db entries
     connection = PG.connect(dbname: 'bookmark_manager_test')
-    bookmark_test = Bookmarks.create(url: 'http://www.makersacademy.com', title: 'Makers Academy');
-    Bookmarks.create(url: 'http://www.destroyallsoftware.com', title: 'Destroy All Software');
-    Bookmarks.create(url: 'http://www.google.com', title: "Google");
+    bookmark_test = Bookmarks.create(url: 'http://www.makersacademy.com', title: 'Makers Academy').first
+    Bookmarks.create(url: 'http://www.destroyallsoftware.com', title: 'Destroy All Software')
+    Bookmarks.create(url: 'http://www.google.com', title: "Google")
     
     # Unit test for Bookmark class
     bookmarks = Bookmarks.all
     expect(bookmarks.size).to eq 3
     expect(bookmarks.first).to be_a Bookmarks
-    #expect(bookmarks.first.id).to eq bookmark_test['id']
+    expect(bookmarks.first.id).to eq bookmark_test['id']
     expect(bookmarks.first.url).to eq "http://www.makersacademy.com"
     expect(bookmarks.first.title).to eq "Makers Academy"
   end
